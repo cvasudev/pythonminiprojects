534e9e1d1eb3072f918f19f958dbca69f5677b7c
cvasudev <40176262+cvasudev@users.noreply.github.com>
2020-03-15 16:57:05 +0530

    Add files via upload

---
+++ GuessNumber.py
@@ -0,0 +1,33 @@
+import random
+
+#variable that will store the value for random number
+rannum = random.randrange(1,10,1)
+
+flag=True
+
+while(flag):
+   try:
+       num = int(input("Please guess the number in the range 1-10 : "))
+       if num not in range(1,11) and num!=12:
+           print("Incorrect Value")
+       elif num == rannum:
+           print("The number is correct !!!")
+           flag=False
+       elif num>rannum and (num-rannum)>3:
+           print("Number is too much greater")
+       elif num>rannum and (num-rannum)<=3:
+           print("The number is greater however you are very close!!!")
+       elif num<rannum and (rannum-num)>3:
+           print("Number is too much less")
+       elif num<rannum and (rannum-num)<=3:
+           print("The number is less however you are very close!!!")
+       elif num==12:
+           break
+   except:
+       print("Invalid entry!!!")
+    
+        
+
+        
+              
+    
