5051cfcadbf526a6ce1d5d61840be91d159f0a87
cvasudev <40176262+cvasudev@users.noreply.github.com>
2020-07-15 21:11:26 +0530

    Update GuessNumber.py

--- GuessNumber.py
+++ GuessNumber.py
@@ -5,8 +5,11 @@ rannum = random.randrange(1,10,1)
 
 flag=True
 
+name = input("Please enter your name : ")
+
 while(flag):
    try:
+       print(f"Hi {name}") 
        num = int(input("Please guess the number in the range 1-10 : "))
        if num not in range(1,11) and num!=12:
            print("Incorrect Value")
